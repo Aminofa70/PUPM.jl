d23e686e954f2b80e662b15995c6bf9f6625afa4
Aminofa70 <amin.alibakhshi@upm.es>
2024-12-13 23:03:26 -0500

    check doc

--- docs/make.jl
+++ docs/make.jl
@@ -6,18 +6,21 @@ DocMeta.setdocmeta!(PUPM, :DocTestSetup, :(using PUPM); recursive=true)
 makedocs(;
     modules=[PUPM],
     authors="Aminofa70 <amin.alibakhshi@upm.es> and contributors",
-    sitename="PUPM.jl",
+    repo = "github.com/Aminofa70/PUPM.jl",
+    sitename= "PUPM.jl",
     format=Documenter.HTML(;
-        canonical="https://Aminofa70.github.io/PUPM.jl",
-        edit_link="main",
+        canonical= "https://Aminofa70.github.io/PUPM.jl",
+        edit_link= "main",
         assets=String[],
     ),
     pages=[
-        "Home" => "index.md",
+        "Home" => "index.md", "Functions" => "index.md",
     ],
+    checkdocs = :none,  # This replaces strict=false# ignores my codes 
 )
 
 deploydocs(;
     repo="github.com/Aminofa70/PUPM.jl",
     devbranch="main",
+    push_preview = false,
 )
--- docs/src/index.md
+++ docs/src/index.md
@@ -4,12 +4,13 @@ CurrentModule = PUPM
 
 # PUPM
 
-Welcome to the documentation for MyProject.jl!
+welcome to my
 Documentation for [PUPM](https://github.com/Aminofa70/PUPM.jl).
 
 ```@index
 ```
 
-```@autodocs
-Modules = [PUPM]
 ```
+function
+```
+
