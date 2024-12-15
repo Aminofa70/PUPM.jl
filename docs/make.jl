374f31f2b3e43cd26b89128383f72bcfbc549ebc
Aminofa70 <amin.alibakhshi@upm.es>
2024-12-14 20:25:27 -0500

    change to doc_4

--- docs/make.jl
+++ docs/make.jl
@@ -1,4 +1,3 @@
-
 using PUPM
 using Documenter
 
@@ -33,34 +32,3 @@ deploydocs(
 
 
 
-
-# using PUPM
-# using Documenter
-
-# DocMeta.setdocmeta!(PUPM, :DocTestSetup, :(using PUPM); recursive=true)
-
-# makedocs(;
-#     modules=[PUPM],
-#     authors= "Aminofa70 <amin.alibakhshi@upm.es> and contributors",
-#     repo = "github.com/Aminofa70/PUPM.jl",
-#     sitename= "PUPM.jl",
-#     format = Documenter.HTML(;
-#         prettyurls= get(ENV, "CI", "false") == "true",
-#         canonical= "https://Aminofa70.github.io/PUPM.jl",
-#         # edit_link= "main",
-#         # assets=String[],
-#     ),
-#     pages=[
-#         "Home" => "index.md", 
-#         #"Functions" => "functions.md",
-#     ],
-#    # checkdocs = :none,  # This replaces strict=false# ignores my codes 
-# )
-
-# deploydocs(;
-#     repo= "github.com/Aminofa70/PUPM.jl",
-#     devbranch ="main",
-#     branch = "gh-pages",
-#     folder = "dev",
-#     push_preview = false,
-# )
