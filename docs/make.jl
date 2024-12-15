db222105758ec0ae1fe875852b8001c9d144f337
Aminofa70 <amin.alibakhshi@upm.es>
2024-12-14 19:51:45 -0500

    load doc

--- docs/make.jl
+++ docs/make.jl
@@ -1,28 +1,65 @@
+
 using PUPM
 using Documenter
 
+# Set up documentation metadata for doctests
 DocMeta.setdocmeta!(PUPM, :DocTestSetup, :(using PUPM); recursive=true)
 
-makedocs(;
-    modules=[PUPM],
-    authors="Aminofa70 <amin.alibakhshi@upm.es> and contributors",
-    repo = "github.com/Aminofa70/PUPM.jl",
-    sitename= "PUPM.jl",
-    format=Documenter.HTML(;
-        prettyurls= get(ENV, "CI", "false") == "true",
-        canonical= "https://Aminofa70.github.io/PUPM.jl",
-        # edit_link= "main",
-        # assets=String[],
+# Build the documentation
+makedocs(
+    modules = [PUPM],
+    authors = "Aminofa70 <amin.alibakhshi@upm.es> and contributors",
+    repo = "https://github.com/Aminofa70/PUPM.jl",
+    sitename = "PUPM.jl",
+    format = Documenter.HTML(
+        prettyurls = get(ENV, "CI", "false") == "true"
     ),
-    pages=[
-        "Home" => "index.md", 
-        "Functions" => "functions.md",
-    ],
-   # checkdocs = :none,  # This replaces strict=false# ignores my codes 
+    pages = [
+        "Home" => "index.md",
+        # Add more pages as needed
+    ]
 )
 
-deploydocs(;
-    repo="github.com/Aminofa70/PUPM.jl",
-    devbranch="main",
-    push_preview = false,
+# Deploy the documentation
+deploydocs(
+    repo = "https://github.com/Aminofa70/PUPM.jl",
+    devbranch = "main",
+    branch = "gh-pages",
+    dirname = "dev",  # Replaces `folder`
+    push_preview = false  # Set to true if previews are required
 )
+
+
+
+
+
+# using PUPM
+# using Documenter
+
+# DocMeta.setdocmeta!(PUPM, :DocTestSetup, :(using PUPM); recursive=true)
+
+# makedocs(;
+#     modules=[PUPM],
+#     authors= "Aminofa70 <amin.alibakhshi@upm.es> and contributors",
+#     repo = "github.com/Aminofa70/PUPM.jl",
+#     sitename= "PUPM.jl",
+#     format = Documenter.HTML(;
+#         prettyurls= get(ENV, "CI", "false") == "true",
+#         canonical= "https://Aminofa70.github.io/PUPM.jl",
+#         # edit_link= "main",
+#         # assets=String[],
+#     ),
+#     pages=[
+#         "Home" => "index.md", 
+#         #"Functions" => "functions.md",
+#     ],
+#    # checkdocs = :none,  # This replaces strict=false# ignores my codes 
+# )
+
+# deploydocs(;
+#     repo= "github.com/Aminofa70/PUPM.jl",
+#     devbranch ="main",
+#     branch = "gh-pages",
+#     folder = "dev",
+#     push_preview = false,
+# )
--- docs/src/index.md
+++ docs/src/index.md
@@ -4,9 +4,12 @@ CurrentModule = PUPM
 
 # PUPM
 
-welcome to my
+Welcome to
 Documentation for [PUPM](https://github.com/Aminofa70/PUPM.jl).
 
+Functions for the package are listed as follows.
+
+In this package, we can use the Penealized Updated Properties Model approach for topology optimization of 2D and 3D structures.
 ```@index
 ```
 
