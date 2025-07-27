02c8c9d011b8374b90226370313a617c4290d7b0
Aminofa70 <amin.alibakhshi@upm.es>
2025-07-27 22:00:44 +0200

    change to docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,35 +1,28 @@
 using PUPM
 using Documenter
+using Documenter.Remotes
 
 # Set up documentation metadata for doctests
 DocMeta.setdocmeta!(PUPM, :DocTestSetup, :(using PUPM); recursive=true)
 
-# Build the documentation
 makedocs(
     modules = [PUPM],
     authors = "Aminofa70 <amin.alibakhshi@upm.es> and contributors",
-    repo = "https://github.com/Aminofa70/PUPM.jl",
+    repo = Remotes.GitHub("Aminofa70", "PUPM.jl"),
     sitename = "PUPM.jl",
     format = Documenter.HTML(
         prettyurls = get(ENV, "CI", "false") == "true"
     ),
     pages = [
         "Home" => "index.md",
-        # Add more pages as needed
+        "Install" => "install.md"
     ]
 )
 
-# Deploy the documentation
 deploydocs(
-    repo = "https://github.com/Aminofa70/PUPM.jl",  # Full HTTPS URL to your repository
-    branch = "gh-pages",                           # GitHub Pages branch
-   # dirname = "stable",                            # Folder for stable docs
-   dirname = "",                                # Folder for development docs
-    devbranch = "main",                            # Development branch
-    push_preview = false                           # Optional: Disable previews
+    repo = "https://github.com/Aminofa70/PUPM.jl",
+    branch = "gh-pages",
+    dirname = "",  # or "dev" if you prefer
+    devbranch = "main",
+    push_preview = false
 )
-
-
-
-
-
